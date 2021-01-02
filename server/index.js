2b2c1280da7f119dd288f5950a174cddf1ddbbe5
ucef8233 <-u>
2021-01-02 12:10:20 +0100

    confic server and add dependencies [express cors body-parser mongoose nodemon]

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
+const app = express()
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+const CONNEXION_URL = 'mongodb+srv://admin:WCPtmjPuHennI9Bo@cluster0.aii3j.mongodb.net/memory-app?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+mongoose.connect(CONNEXION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server listning in PORT ${PORT}`)
+    })
+  })
+  .catch((error) => console.log(error.messag))
+mongoose.set("useFindAndModify", false)
